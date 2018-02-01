c0be5a5060a15fecfc8d48164d14ce6dea547c0d
Dustin LeGars <dlegars1976@gmail.com>
2018-02-01 10:25:22 -0500

    Initial Things and such.

---
+++ ArrayPrograms/QuizicalProgramming.java
@@ -0,0 +1,22 @@
+/*
+Java <Program Identifier>
+Programming With Java Period 3
+Written in Atom 21-ish x64
+<Compiler>
+Written By Dustin LeGars
+Written on <date> at <time>
+{>===----===<}
+Intent:
+  <what is to be fulfilled by the program>
+{>===----===<}
+<GitHub Link>
+*/
+
+
+public class Program {
+
+  public static void main(String[] args) {
+    
+  }
+
+}
---
+++ Templates/JavaProgramTemplate.java
@@ -0,0 +1,22 @@
+/*
+Java <Program Identifier>
+Programming With Java Period 3
+Written in Atom <version> x64
+<Compiler>
+Written By Dustin LeGars
+Written on <date> at <time>
+{>===----===<}
+Intent:
+  <what is to be fulfilled by the program>
+{>===----===<}
+<GitHub Link>
+*/
+
+
+public class Program {
+
+  public static void main(String[] args) {
+    
+  }
+
+}
