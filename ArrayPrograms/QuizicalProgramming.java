ae98c09c3c7bddad2af842df63340c9f9f78cb9b
Dustin LeGars <dlegars1976@gmail.com>
2018-02-02 09:56:53 -0500

    Java Program Rep2-Prog1

--- ArrayPrograms/QuizicalProgramming.java
+++ ArrayPrograms/QuizicalProgramming.java
@@ -1,22 +1,36 @@
 /*
-Java <Program Identifier>
+Java Array Program for Quiz Scoring
 Programming With Java Period 3
-Written in Atom 21-ish x64
-<Compiler>
-Written By Dustin LeGars
+  <Editor>  | Written in Atom 1.23.3 x64
+ <Compiler> |
+<Programmer>| Dustin LeGars
 Written on <date> at <time>
 {>===----===<}
 Intent:
-  <what is to be fulfilled by the program>
+  Write a program where the user can enter three grades for three different students using arrays.
 {>===----===<}
-<GitHub Link>
+https://github.com/Doosty295/Java-Programs-Two--Electric-Boogaloo/blob/master/ArrayPrograms/QuizicalProgramming.java
 */
-
+import java.util.Scanner;
 
 public class Program {
 
   public static void main(String[] args) {
-    
+
+    int[][] students = new int[3][3];
+
+    Scanner input = new Scanner(System.in);
+
+    //Inputstuff
+System.out.println("Hello! You will be asked to input");
+    for (int ct = 0; ct > 3; ct++) {
+      for (int ct = 0; ct > 3 ; ct++) {
+
+      }
+      
+    }
+
+
   }
 
 }
