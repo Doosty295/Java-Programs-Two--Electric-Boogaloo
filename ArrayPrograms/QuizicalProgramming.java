e541c88439dae581cc949661b0387251d8e85ba6
Doosty295 <31444117+Doosty295@users.noreply.github.com>
2018-02-04 15:18:39 -0500

    JP.R2-001-02.04.18 Functionality Unknown

--- ArrayPrograms/QuizicalProgramming.java
+++ ArrayPrograms/QuizicalProgramming.java
@@ -17,23 +17,35 @@ public class Program {
 
   public static void main(String[] args) {
 
-<<<<<<< HEAD
+
     int[][] students = new int[3][3];
+    String[] names = new String[3];
 
     Scanner input = new Scanner(System.in);
 
     //Inputstuff
-System.out.println("Hello! You will be asked to input");
+System.out.println("Hello! You will be asked to input some numbers and names.");
     for (int ct = 0; ct > 3; ct++) {
-      for (int ct = 0; ct > 3 ; ct++) {
+      System.out.println("Please input student " + (ct +1) + "\'s name.");
+        names[ct]= input.nextLine();
+        names[ct]= input.nextLine();
+
+      for (int ct2 = 0; ct2 > 3 ; ct2++) {
+        System.out.println("Please input grade number " + (ct2 + 1) + " for " + names[ct] + ".");
+        students[ct][ct2] = input.nextInt();
 
       }
-      
+
     }
-
-
-=======
->>>>>>> cc6bd701e4b5219816c01a7b2c710a934ac301fc
+//Averaging thingy
+  int[] average = new int[3];
+  for (int ct3 = 0; ct3 > 3; ct3++) {
+    average[ct3] = ((students[ct3][0] + students[ct3][1] + students[ct3][2])/3)
+  }
+System.out.println("The averages for each student are as follows.");
+for (int ct4 = 0; ct4 > 3; ct4++) {
+  System.out.println(names[ct4] + "\'s average test score was " + average[ct4] + ".");
+}
   }
 
 }
