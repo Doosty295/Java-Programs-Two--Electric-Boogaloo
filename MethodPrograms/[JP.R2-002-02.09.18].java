a46156d4e7ac5101cbd411b417fe75d55214011a
Dustin LeGars <dlegars1976@gmail.com>
2018-02-09 14:45:27 -0500

    [JP.R2-002-02.09.18]
    Banking with Methods!

---
+++ MethodPrograms/[JP.R2-002-02.09.18].java
@@ -0,0 +1,36 @@
+/*
+Title: [Program Title]
+  Intent:  [Purpose of Program]
+  Created: [02/09/2018, 14:40]
+  Updated: [02/06/2018, 10:00]
+Programmer: LeGars, Dustin
+  Editor: Atom 1.23.3 x64
+  Compiler: [Compiling Program]
+Program Identifier: [JP.R2-002-02.09.18]
+Status: [Incomplete/Complete/Deprecated]
+  Progress:    [--------------------] 0%
+    Writing:   [----------] 0%
+    Compiling: [----------] 0%
+    Debugging: [----------] 0%
+    Err Check: [----------] 0%
+Imports: 1
+  Scanner
+GitHub Link: [Link]
+Additional Information: Banking Stuff
+
+
+
+
+*/
+
+
+
+public class Program {
+
+  public static void main(String[] args) {
+
+
+
+  }
+
+}
