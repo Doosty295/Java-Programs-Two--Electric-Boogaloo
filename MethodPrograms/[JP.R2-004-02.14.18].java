a265bc7e36adbc8f69d086f594799ca20afef9b6
Doosty295 <31444117+Doosty295@users.noreply.github.com>
2018-02-14 16:30:12 -0500

    [JP.R2-004-02.14.18] created

---
+++ MethodPrograms/[JP.R2-004-02.14.18].java
@@ -0,0 +1,38 @@
+/*
+Title: [Prime Number Game]
+  Intent:  [Test ability to use Java]
+  Created: [02/14/2018, 16:25]
+  Updated: [02/14/2018, 16:25]
+Programmer: LeGars, Dustin
+  Editor: Atom 1.24.0 x64
+  Compiler: [None]
+Program Identifier: [JP.R2-004-02.14.18]
+Status: [Incomplete]
+  Progress:    [--------------------] 0%
+    Writing:   [----------] 0%
+    Compiling: [----------] NULL
+    Debugging: [----------] NULL
+    Err Check: [----------] NULL
+Imports: [One (1)]
+  [Scanner]
+GitHub Link: [Link]
+Additional Information:
+Testing Programming Skills in:
+  Input Validation
+  Looping Structures
+  Decision Structures
+  Random Number Generation
+
+*/
+
+
+
+public class Program {
+
+  public static void main(String[] args) {
+
+
+
+  }
+
+}
