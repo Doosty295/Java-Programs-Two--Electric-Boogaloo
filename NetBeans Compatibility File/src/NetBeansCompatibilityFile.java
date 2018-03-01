f5f374219a206902de438cca0bafa87e6ee20a63
Dustin LeGars <dlegars1976@gmail.com>
2018-03-01 10:02:28 -0500

    NBCompatFile
    For compiling files in netbeans that weren't written in NB.

---
+++ NetBeans Compatibility File/src/NetBeansCompatibilityFile.java
@@ -0,0 +1,18 @@
+/*
+ This is here for testing code that wasn't initially compiled with net beans.
+ Because convienent
+
+
+ */
+package netbeans.compatibility.file;
+
+
+public class NetBeansCompatibilityFile {
+
+    public static void main(String[] args) {
+
+
+
+    }
+
+}
