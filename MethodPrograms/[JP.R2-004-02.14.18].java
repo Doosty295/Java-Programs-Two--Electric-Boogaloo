74464deaec091675aab761e3e7c0df86ab59ac07
Dustin LeGars <dlegars1976@gmail.com>
2018-03-01 08:48:29 -0500

    [JP.R2-004-02.14.18] updated

--- MethodPrograms/[JP.R2-004-02.14.18].java
+++ MethodPrograms/[JP.R2-004-02.14.18].java
@@ -39,12 +39,27 @@ public class Program {
       double numGenB = (numGenA*10);
       int queryNum = (int)numGenB;
       int contin = 0;
+      int response = 0;
+      boolean respBool;
+      
+      //Prime Checker
+      //checks whether an int is prime or not.
+      boolean isPrime(queryNum) {
+          //check if n is a multiple of 2
+          if (queryNum%2==0) return false;
+          //if not, then just check the odds
+          for(int i=3;i*i<=queryNum;i+=2) {
+              if(queryNum%i==0)
+                  return false;
+          }
+          return true;
+      }
 
     //Interaction
       System.out.println("Hello and welcome to the Second Edition  of the PrimeGame!");
       System.out.println("Please select one of the following opitons.\n");
-      System.out.println("To commence with the game, enter the number '1'.");
-      System.out.println("To Terminate this programs operation, enter the number '01010001'.");
+      System.out.println("To commence with the game, enter the number \'1\'.");
+      System.out.println("To Terminate this programs operation, enter the number \'01010001\'.");
       contin = input.nextInt();
 
       if (contin == 1) {
@@ -52,8 +67,20 @@ public class Program {
           for (int loop; loop > 20; loop++) {
             System.out.println("_");
           }
+          System.out.println("There is a number. This number is " + queryNum + ".");
+          System.out.println("Is the number, " + queryNum + ", a prime number?");
+          System.out.println("Note: Prime numbers are divisible by only themselves.");
+          System.out.println("If you believe the number to be prime, press \'1\'. If you believe the number to be non-prime, press \'2\'");
+          response = input.nextInt();
 
-          
+          if () {
+
+          }
+
+
+
+
+
         } while ();
       }
       }
