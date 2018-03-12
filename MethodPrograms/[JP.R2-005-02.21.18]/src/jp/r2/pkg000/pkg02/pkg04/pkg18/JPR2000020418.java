fda47422be025cae3447714a7e38291f0d61f9c5
Dustin LeGars <dlegars1976@gmail.com>
2018-03-12 10:20:45 -0400

    update

--- MethodPrograms/[JP.R2-005-02.21.18]/src/jp/r2/pkg000/pkg02/pkg04/pkg18/JPR2000020418.java
+++ MethodPrograms/[JP.R2-005-02.21.18]/src/jp/r2/pkg000/pkg02/pkg04/pkg18/JPR2000020418.java
@@ -30,30 +30,68 @@ public class JPR2000020418 {
 
   public static void main(String[] args) {
 
-    char c1r1 = ' ';
-    char c1r2 = ' ';
-    char c1r3 = ' ';
-    char c2r1 = ' ';
-    char c2r2 = ' ';
-    char c2r3 = ' ';
-    char c3r1 = ' ';
-    char c3r2 = ' ';
-    char c3r3 = ' ';
+    int c1r1 = 0;
+    int c1r2 = 0;
+    int c1r3 = 0;
+    int c2r1 = 0;
+    int c2r2 = 0;
+    int c2r3 = 0;
+    int c3r1 = 0;
+    int c3r2 = 0;
+    int c3r3 = 0;
 
     Scanner input = new Scanner(System.in);
 
-    System.out.println("  --A---B---C--");
-    System.out.println("A | " + c1r1 + " | " + c2r1 + " | " + c3r1 + " |");
+    System.out.println("  --1---2---3--");
+    System.out.println("1 | " + c1r1 + " | " + c2r1 + " | " + c3r1 + " |");
     System.out.println("  -------------");
-    System.out.println("B | " + c1r2 + " | " + c2r2 + " | " + c3r2 + " |");
+    System.out.println("2 | " + c1r2 + " | " + c2r2 + " | " + c3r2 + " |");
     System.out.println("  -------------");
-    System.out.println("C | " + c1r3 + " | " + c2r3 + " | " + c3r3 + " |");
+    System.out.println("3 | " + c1r3 + " | " + c2r3 + " | " + c3r3 + " |");
     System.out.println("  -------------");
 
 
-    System.out.println("Pick a column to mark. (A, B, C)");
-
-    System.out.println("Pick a row to mark. (A, B, C)");
+    System.out.println("Pick a column to mark. (1, 2, 3)");
+    int colChoice = input.nextInt();
+    if (colChoice == 1) {
+      System.out.println("Pick a row to mark. (1, 2, 3)");
+      int rowChoice = input.nextInt();
+      if (rowChoice == 1) {
+        c1r1 = 1;
+      }
+      if (rowChoice == 2) {
+        c1r2 = 1;
+      }
+      if (rowChoice == 3) {
+        c1r3 = 1;
+      }
+    }
+    else if (colChoice == 2) {
+      System.out.println("Pick a row to mark. (1, 2, 3)");
+      int rowChoice = input.nextInt();
+      if (rowChoice == 1) {
+        c2r1 = 1;
+      }
+      if (rowChoice == 2) {
+        c2r2 = 1;
+      }
+      if (rowChoice == 3) {
+        c2r3 = 1;
+      }
+    }
+    else if (colChoice == 3) {
+      System.out.println("Pick a row to mark. (1, 2, 3)");
+      int rowChoice = input.nextInt();
+      if (rowChoice == 1) {
+        c3r1 = 1;
+      }
+      if (rowChoice == 2) {
+        c3r2 = 1;
+      }
+      if (rowChoice == 3) {
+        c3r3 = 1;
+      }
+    }
 
 
   }
---
+++ MethodPrograms/[JP.R2-005-02.21.18]/src/jp/r2/pkg000/pkg02/pkg04/pkg18/method.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jp.r2.pkg000.pkg02.pkg04.pkg18;
+
+/**
+ *
+ * @author 9djl001
+ */
+class method Boxen(){
+     
+  System.out.println("  --1---2---3--");
+  System.out.println("1 | " + c1r1 + " | " + c2r1 + " | " + c3r1 + " |");
+  System.out.println("  -------------");
+  System.out.println("2 | " + c1r2 + " | " + c2r2 + " | " + c3r2 + " |");
+  System.out.println("  -------------");
+  System.out.println("3 | " + c1r3 + " | " + c2r3 + " | " + c3r3 + " |");
+  System.out.println("  -------------");
+
+}
